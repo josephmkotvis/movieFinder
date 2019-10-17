db6e3b9ee546da43084618db2ebf1653ad670a7d
Jmkotvis <josephmkotvis@gmail.com>
2019-10-16 21:35:43 -0500

    App.js Index.js fix

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import Landing from './components/layout/Landing';
 
 
-const App = () => {
+const App = () => (
   <Provider store={store}>
     <Router>
       <Fragment>
         <Switch>
-          <Route exact path="/" component={} />
-
+          <Route exact path="/" component={Landing} />
           {/* For 404 Not Found */}
           {/* <Route exact path="*" component={} /> */}
         </Switch>
@@ -20,6 +20,6 @@ const App = () => {
     </Router>
 
   </Provider>
-}
+)
 
 export default App;
---
+++ src/components/layout/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import {Link} from 'react-router-dom';
+
+const Landing = () => {
+    return (
+    <section>
+        !!!
+
+    </section>
+    )
+}
+
+export default Landing
