54694b2e499112b8e11561f2e2559ab85e6d5596
Jmkotvis <josephmkotvis@gmail.com>
2019-10-16 21:30:13 -0500

    Boiler Plate for Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+const App = () => {
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={} />
+
+          {/* For 404 Not Found */}
+          {/* <Route exact path="*" component={} /> */}
+        </Switch>
+      </Fragment>
+    </Router>
+
+  </Provider>
 }
 
 export default App;
